1b2f7e5c482d55adc7e0dfbe724deb44364aeec7
diogoloiola <diogoloiola666@gmail.com>
2021-06-08 10:33:43 -0300

    rotas atualizadas, e utilizando os dois banco de dados

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
-const db = require('../database/db')
+const databases = require('../database/db')
 const countDt = require('../source/countDt')
 const functions = require('../source/generateQuery')
-
+const routerDb = require('../source/routerDb')
 
 const generateQuery = functions[0]
 const createJson = functions[1]
 const score = functions[2]
 
 var express = require('express');
-const { reset } = require('nodemon')
 var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/repository/:id', function(req, res, next) {
+router.get('/repository/:id/database/:type', function(req, res, next) {
+
+    const type = req.params.type
+    const db = new routerDb(databases[0], databases[1]).getDatabase(type)
+
     db.any(generateQuery(req.params.id))
         .then(function(data) {
             res.json(data)
@@ -23,13 +26,22 @@ router.get('/repository/:id', function(req, res, next) {
         })
 });
 
-router.get('/projects', function(req, res, next) {
+router.get('/projects/:type', function(req, res, next) {
+
+    const type = req.params.type
+    const db = new routerDb(databases[0], databases[1]).getDatabase(type)
+
+
     db.any('select * from projects')
         .then(data => res.json(data))
         .catch(data => res.json(data))
 });
 
-router.get('/dt/project/:id', function(req, res, next) {
+router.get('/dt/project/:id/database/:type', function(req, res, next) {
+
+    const type = req.params.type
+    const db = new routerDb(databases[0], databases[1]).getDatabase(type)
+
     db.any(generateQuery(req.params.id))
         .then(function(data) {
             res.json(countDt(data))
@@ -38,7 +50,11 @@ router.get('/dt/project/:id', function(req, res, next) {
         })
 });
 
-router.get('/jsonHierarchical/:id', function(req, res, next) {
+router.get('/jsonHierarchical/:id/database/:type', function(req, res, next) {
+
+    const type = req.params.type
+    const db = new routerDb(databases[0], databases[1]).getDatabase(type)
+
     const idProject = req.params.id
     db.multi(generateQuery(idProject) + ';' + score(idProject)).then((projects, classes) => {
         res.json(createJson(projects[0], projects[1]))
