8de741575b037f54b4455aa2e72238f41bec479a
diogoloiola <diogomouraloiola@gmail.com>
2021-09-07 20:33:28 -0300

    Casos de teste alterado, mas ainda não foi nada feito

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -1,8 +1,19 @@
 import { describe, test, expect } from '@jest/globals';
 
 
-describe('#Testando 1,2,3', () => {
-    test('#teste teste', () => {
-        expect(true).toBeTruthy()
+describe('#Utils', () => {
+    describe('#Test for countDT', () => {
+        test('#Checks if the object format is correct', () => {
+            const result = {
+                "architecture debt": 1,
+                "build debt": 1,
+                "code debt": 1,
+                "requirement debt": 1,
+                "defect debt": 1,
+                "design debt": 1,
+                "documentation debt": 1,
+                "test debt": 0
+            }
+        })
     })
 })
