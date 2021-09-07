ea328ae7b1f41d264c5ee64a33fc7ceccf112898
diogoloiola <diogomouraloiola@gmail.com>
2021-09-07 20:35:18 -0300

    Deletando arquivos que não fazem parte do projeto

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const pgp = require('pg-promise')( /* options */ )
-const noHeurisct = pgp('postgres://postgres:1234@localhost:5432/tccFinalSem')
-noHeurisct.connect()
-
-const withHeurisct = pgp('postgres://postgres:1234@localhost:5432/comheuristica')
-withHeurisct.connect()
-
-
-module.exports = [noHeurisct, withHeurisct]
