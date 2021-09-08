2d85084db4be70b8a3943eed1a9caf8149b54faa
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 16:56:42 -0300

    Importação da conexão com o banco atualizada

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { conection } from '../database/conection.js';
+import { connection } from '../database/connection.js';
 
 const router = express.Router();
 
 import Handler from '../src/Handler.js';
 
-const handler = new Handler(conection);
+const handler = new Handler(connection);
 
 router.get('/repository/:id', (request, response) => handler.getTechnicalDebtFromRepository(request, response));
 router.get('/projects', async (request, response) => handler.getProjects(request, response));
