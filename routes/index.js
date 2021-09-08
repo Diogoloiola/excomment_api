ca9ca844c9df35276d3a15e24f18e70df5e068a3
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 16:39:18 -0300

    Criada a rota que retorna os projetos que estão na base de dados

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,5 @@ import Handler from '../src/Handler.js';
 const handler = new Handler(conection);
 
 router.get('/repository/:id', (request, response) => handler.getTechnicalDebtFromRepository(request, response));
-
+router.get('/projects', async (request, response) => handler.getProjects(request, response));
 export default router;
