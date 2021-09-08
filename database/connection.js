8211af07ada232a46956a2fae82e2bc8f5e85e3d
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 20:24:03 -0300

    Mudada a forma como é feita a conexão com o banco de dados

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,24 @@
 import pgp from 'pg-promise';
-const connection = pgp()('postgres://postgres:1234@localhost:5432/with_heuristics')
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+/**
+ * This function returns a url to connect to the database.
+ * @returns string
+ */
+
+function formatStringFoConnection() {
+    const password = process.env.DB_PASS;
+    const user = process.env.DB_USER;
+    const host = process.env.DB_HOST;
+    const port = process.env.DB_PORT;
+    const database = process.env.DB_NAME;
+    const url = `postgres://${user}:${password}@${host}:${port}/${database}`;
+
+    return url;
+}
+const connection = pgp()(formatStringFoConnection())
 connection.connect()
 
 export { connection };
