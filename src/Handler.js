902dd4b63346da000a349541685b039df51d518a
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 16:45:04 -0300

    Adicionada documentação para as funções já implementadas

--- src/Handler.js
+++ src/Handler.js
@@ -1,9 +1,19 @@
 import { queryToGetAmountTD } from './utilsQuery.js';
 
 export default class Handler {
+    /**
+     * Constructor class
+     * @param {*} connection
+     */
     constructor(connection) {
         this.connection = connection
     }
+    /**
+     * This function return projects in database
+     * @param {*} request
+     * @param {*} response
+     * @return {Object}
+     */
     async getProjects(request, response) {
         try {
             const data = await this.connection.any('select * from projects');
@@ -12,6 +22,12 @@ export default class Handler {
             response.json(error);
         }
     }
+    /**
+     * This function returns the td present in the project
+     * @param {*} request
+     * @param {*} response
+     * @return {Object}
+     */
     getTechnicalDebtFromRepository(request, response) {
         const { id } = request.params;
         if (id !== undefined) {
