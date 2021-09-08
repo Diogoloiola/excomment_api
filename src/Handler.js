9d7d59513828947fc492b4d297eab5a96d49247d
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 19:13:24 -0300

    Adicionada a consulta que retorna os scores presentes nos comenatários

--- src/Handler.js
+++ src/Handler.js
@@ -1,52 +1,92 @@
-import { queryToGetAmountTD } from './utilsQuery.js';
-
+import { queryToGetAmountTD, queryToGetScores } from "./utilsQuery.js";
+import { dtCountInProject } from "./utils.js";
 export default class Handler {
-    /**
-     * Constructor class
-     * @param {*} connection
-     */
-    constructor(connection) {
-        this.connection = connection
+  /**
+   * Constructor class
+   * @param {*} connection
+   */
+  constructor(connection) {
+    this.connection = connection;
+  }
+  /**
+   * This function return projects in database
+   * @param {*} request
+   * @param {*} response
+   * @return {Object}
+   */
+  async getProjects(request, response) {
+    try {
+      const data = await this.connection.any("select * from projects");
+      response.json(data);
+    } catch (error) {
+      response.json(error);
     }
-    /**
-     * This function return projects in database
-     * @param {*} request
-     * @param {*} response
-     * @return {Object}
-     */
-    async getProjects(request, response) {
-        try {
-            const data = await this.connection.any('select * from projects');
-            response.json(data)
-        } catch (error) {
-            response.json(error);
-        }
+  }
+  /**
+   * This function returns the td present in the project
+   * @param {*} request
+   * @param {*} response
+   * @return {Object}
+   */
+  getTechnicalDebtFromRepository(request, response) {
+    const { id } = request.params;
+    if (id !== undefined) {
+      this.connection.any(queryToGetAmountTD(id)).then((data) => {
+        response.json(data);
+      });
+    } else {
+      response.json({
+        msg: "Algo deu errado",
+        status: 400,
+      });
     }
-    /**
-     * This function returns the td present in the project
-     * @param {*} request
-     * @param {*} response
-     * @return {Object}
-     */
-    getTechnicalDebtFromRepository(request, response) {
-        const { id } = request.params;
-        if (id !== undefined) {
-            this.connection.any(queryToGetAmountTD(id)).then(data => {
-                response.json(data)
-            });
-        } else {
-            response.json({
-                'msg': 'Algo deu errado',
-                'status': 400
-            })
-        }
-    }
+  }
 
-    getTechnicalDebtCount(idProject) {
-        //TODO: ainda por fazer
-    }
+  /**
+   * This function returns the td amount present in the project
+   * @param {*} request
+   * @param {*} response
+   * @return {Object}
+   */
 
-    getScoreFromRepository(idProject) {
-        //TODO: ainda por fazer
+  async getTechnicalDebtCount(request, response) {
+    const { id } = request.params;
+    console.log(id);
+    if (id !== undefined) {
+      try {
+        const data = await this.connection.any(queryToGetAmountTD(id));
+        const result = dtCountInProject(data);
+        response.json(result);
+      } catch (error) {
+        response.json(error);
+      }
+    } else {
+      response.json({
+        msg: "Algo deu errado",
+        status: 400,
+      });
     }
+  }
+  /**
+   * This function returns the score of a project
+   * @param {*} request
+   * @param {*} response
+   */
+  async getScoreFromRepository(request, response) {
+    //TODO: still does not work
+    const { id } = request.params;
+    if (id !== undefined) {
+      try {
+        const data = this.connection.any(queryToGetScores(id));
+        response.json(data);
+      } catch (error) {
+        response.json(error);
+      }
+    } else {
+      response.json({
+        msg: "Algo deu errado",
+        status: 400,
+      });
+    }
+  }
 }
