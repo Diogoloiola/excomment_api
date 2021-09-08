505648a2edabbf1f2d9dfc19000bcb61599870ce
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-08 19:13:53 -0300

    Criada a rota que retorna os score dos comentários

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ const handler = new Handler(connection);
 
 router.get('/repository/:id', (request, response) => handler.getTechnicalDebtFromRepository(request, response));
 router.get('/projects', async (request, response) => handler.getProjects(request, response));
+router.get('/amountTD/:id', async (request, response) => handler.getTechnicalDebtCount(request, response));
+/*
+  TODO: needs the bank that has heuristics
+  router.get('/get/score/:id', async (request, response) => handler.getScoreFromRepository(request, response));
+*/
 export default router;
