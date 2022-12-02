0b967d5b72afc3ffd165fcfe70da5cc01f7aa037
Diogo Loiola <diogomouraloiola@gmail.com>
2022-12-02 16:46:13 -0300

    create endpoint to return debts to project

---
+++ app/views/web/v1/projects/_debt.json.jbuilder
@@ -0,0 +1,4 @@
+json.id_comment debt['idcomment']
+json.tdtype debt['tdtype']
+json.comment debt['comment']
+json.name debt['name']
--- config/routes/web.rb
+++ config/routes/web.rb
@@ -1,5 +1,7 @@
 namespace :web, defaults: { format: :json } do
   namespace :v1 do
-    resources :projects, only: %i[show index]
+    resources :projects, only: %i[show index] do
+      get 'debts', on: :member
+    end
   end
 end
