630cf1c39a4c1bafdce5d40a8351e5bb37d8be8e
Diogo Loiola <diogomouraloiola@gmail.com>
2022-12-03 16:12:34 -0300

    create service that return HierarchicalJson

--- app/controllers/web/v1/projects_controller.rb
+++ app/controllers/web/v1/projects_controller.rb
@@ -3,7 +3,7 @@
 module Web
   module V1
     class ProjectsController < ApplicationController # rubocop:disable Style/Documentation
-      before_action :set_project, only: %i[show debts amount_technical_debt]
+      before_action :set_project, only: %i[show debts amount_technical_debt hierarchical_json]
 
       def index
         @projects = Project.all
@@ -25,6 +25,12 @@ module Web
 
       def amount_technical_debt
         render json: { amount: @project.amount_tecnical_debt_for_type }, status: :ok
+      rescue StandardError => e
+        render json: e.message, status: :unprocessable_entity
+      end
+
+      def hierarchical_json
+        render json: HierarchicalJsonService.new(@project.id).call, status: :ok
       rescue StandardError => e
         render json: e.message, status: :unprocessable_entity
       end
--- config/routes/web.rb
+++ config/routes/web.rb
@@ -4,6 +4,7 @@ namespace :web, defaults: { format: :json } do
       get 'debts', on: :member
       get 'scores', on: :collection
       get 'amount_technical_debt', on: :member
+      get 'hierarchical_json', on: :member
     end
   end
 end
